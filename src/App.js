68a06e89bd8a3565d4ea221ad1e96b6cd0419c44
Sanoj Kumar <sanojsanoj638@gmail.com>
2022-06-29 18:04:53 +0530

    login part

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import './App.css';
+import Navbar from "./components/Navbar";
+import "./App.css";
+import LoginFirstPage from "./pages/LoginFirstPage"
+import Login from "./pages/Login";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const getUser = () => {
+      fetch("http://localhost:5000/auth/login/success", {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Credentials": true,
+        },
+      })
+        .then((response) => {
+          if (response.status === 200) return response.json();
+          throw new Error("authentication has been failed!");
+        })
+        .then((resObject) => {
+          setUser(resObject.user);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    getUser();
+  }, []);
+
   return (
-    <div className="App">
-     AIRBNB Clone
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navbar user={user} />
+        <Routes>
+          <Route path="/" element={<LoginFirstPage />} />
+          <Route path="/login" element={user ? <LoginFirstPage /> : <Login />}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/pages/LoginFirstPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './loginFirstPage.css'
+
+
+const LoginFirstPasge = () => {
+  return (
+    <div className='login_first_container'>
+      <a href="Login">Log in</a>
+      <a href="Login" className="signup">Sign up</a>
+      <a href="">Host your home</a>
+      <a href="">Host your Experiance</a>
+      <a href="">Help</a>
+    </div>
+  )
+}
+
+export default LoginFirstPasge
